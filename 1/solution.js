92877be2707cfb741e6ab0c6c606fff55b5b2643
George K. <george@steelcrew.org>
2024-12-01 15:10:35 -0500

    Whoops, forgot part 2

--- 1/solution.js
+++ 1/solution.js
@@ -6,7 +6,20 @@ input.split("\n").forEach((line) => {
 });
 a.sort();
 b.sort();
+a.map(parseInt);
+b.map(parseInt);
+
+// Part 1
 a.forEach((num, i) => {
-    sum += Math.abs(parseInt(num) - parseInt(b[i]))
+    sum += Math.abs(num - b[i]);
+});
+console.log(sum);
+
+// Part 2
+sum = 0;
+a.forEach((num) => {
+    let n = 0;
+    b.forEach((num2) => num == num2 ? n++ : 0);
+    sum += num * n;
 });
 console.log(sum);
